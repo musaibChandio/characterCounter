c3f2d829ca6cd90ce4c4fb786c6ab58822e3e7eb
musaibChandio <musaibchandio000@gmail.com>
2024-11-09 20:25:03 +0500

    character_Counter

---
+++ script.js
@@ -0,0 +1,18 @@
+const textArea = document.getElementById("textarea");
+const totalChar = document.getElementById("total-char");
+const remainChar = document.getElementById("remaining-char");
+
+
+
+textArea.addEventListener("keyup",()=>{
+    console.log("pressed");
+    updateCounter()
+})
+
+function updateCounter(){
+    totalChar.innerText = textArea.value.length
+    // remainChar.innerText -= textArea.value.length 
+    remainChar.innerText = (textArea.getAttribute("maxlength") - textArea.value.length)
+    
+}
+
